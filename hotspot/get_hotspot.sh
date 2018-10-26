d16971e68fe7413be2e9e3f3deb7922000edd084
Mark Stoodley <mstoodle@ca.ibm.com>
2018-10-26 17:48:09 -0400

    Add hotspot directory for fetching hotspot runtime
    Uses adoptopenjdk docker image of hotspot
    
    Signed-off-by: Mark Stoodley <mstoodle@ca.ibm.com>

---
+++ hotspot/get_hotspot.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -f
+#
+# Copyright (c) 2017, 2018 IBM Corp. and others
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which accompanies this
+# distribution and is available at https://www.eclipse.org/legal/epl-2.0/
+# or the Apache License, Version 2.0 which accompanies this distribution and
+# is available at https://www.apache.org/licenses/LICENSE-2.0.
+#
+# This Source Code may also be made available under the following
+# Secondary Licenses when the conditions for such availability set
+# forth in the Eclipse Public License, v. 2.0 are satisfied: GNU
+# General Public License, version 2 with the GNU Classpath
+# Exception [1] and GNU General Public License, version 2 with the
+# OpenJDK Assembly Exception [2].
+#
+# [1] https://www.gnu.org/software/classpath/license.html
+# [2] http://openjdk.java.net/legal/assembly-exception.html
+#
+# SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+
+# just grab adoptopenjdk's jdk8 docker container and copy SDK image out of it
+sudo docker run -v $PWD/j2sdk-image-hotspot:/j2sdk-image-hotspot -t adoptopenjdk/openjdk8 /bin/sh -c "/bin/cp -r /opt/java/openjdk/* /j2sdk-image-hotspot"
